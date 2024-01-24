e6a5786b3689dc4dcd95e8e756558b14f2eb7779
Epsilon16 <marc.nebout@e-artsup.net>
2024-01-24 10:55:04 +0100

    PREMIER PUSH : ajout du début du suika

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private BoxCollider2D _boundaries;
+    [SerializeField] private Transform _fruitThrowTransform;
+
+    private Bounds _bounds;
+
+    private float _leftBound;
+    private float _rightBound;
+
+    private float _startingLeftBound;
+    private float _startingRightBound;
+
+    private float offset;
+
+    private void Awake()
+    {
+        _bounds = _boundaries.bounds;
+
+        offset = transform.position.x - _fruitThrowTransform.position.x;
+
+        _leftBound = _bounds.min.x + offset;
+        _rightBound = _bounds.max.x + offset;
+
+        _startingLeftBound = _leftBound;
+        _startingRightBound = _rightBound;
+    }
+
+    private void Update()
+    {
+        Vector3 newPosition = transform.position + new Vector3(UserInput.MoveInput.x * _moveSpeed * Time.deltaTime, 0f, 0f);
+        newPosition.x = Mathf.Clamp(newPosition.x, _leftBound, _rightBound);
+
+        transform.position = newPosition;
+    }
+}
